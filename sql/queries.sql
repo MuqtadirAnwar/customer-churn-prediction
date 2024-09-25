9bb0c766accea63a67d04d4790d990593d2c2b56
Muqtadir Anwar <muqanwar@gmail.com>
2024-09-25 15:22:39 -0500

    added new queries to calculate order value by age group

--- sql/queries.sql
+++ sql/queries.sql
@@ -4,3 +4,17 @@ FROM customers
 WHERE LoginFrequency = 'Monthly'
   AND DaysSinceLastPurchase > 60
 ORDER BY DaysSinceLastPurchase DESC;
+
+-- Calculate average order value by age group
+SELECT
+  CASE
+    WHEN Age BETWEEN 18 AND 25 THEN '18-25'
+    WHEN Age BETWEEN 26 AND 35 THEN '26-35'
+    WHEN Age BETWEEN 36 AND 45 THEN '36-45'
+    WHEN Age BETWEEN 46 AND 55 THEN '46-55'
+    ELSE '55+'
+  END AS AgeGroup,
+  AVG(AvgOrderValue) AS AvgOrderValue
+FROM customers
+GROUP BY AgeGroup
+ORDER BY AvgOrderValue DESC;
